b225fb67d56b56d47fab73dadc3c7d19ba6a3120
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-03-05 15:25:33 +0400

    Initial commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public int rangeNum(int lowerLimit, int upperLimit) {
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= lowerLimit && i * i <= upperLimit) {
+                counter = counter + 1;
+            }
+        }
+        return counter;
+    }
+}
