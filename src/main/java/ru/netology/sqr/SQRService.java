7e0e2900544a87d8946f75fc7c3d1264297a323c
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-03-09 19:19:08 +0400

    fix remarks2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,7 +2,7 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int rangeNum(int lowerLimit, int upperLimit) {
+    public int rangenum(int lowerLimit, int upperLimit) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= lowerLimit && i * i <= upperLimit) {
